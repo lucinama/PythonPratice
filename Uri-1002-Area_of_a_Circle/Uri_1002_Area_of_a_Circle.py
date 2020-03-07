57cd17e7bc616f140ddc030f57c81f5bdb011eae
Luciana Magno de Almeida <lucianamagnoalmeida@gmail.com>
2020-03-07 18:47:47 -0300

    Simplified code

--- Uri-1002-Area_of_a_Circle/Uri_1002_Area_of_a_Circle.py
+++ Uri-1002-Area_of_a_Circle/Uri_1002_Area_of_a_Circle.py
@@ -1,8 +1,3 @@
-def circle(R):
-    pi = 3.14159
-    A = pi*(R**2)
-    return A
-
-inR = float(input())
-A = circle(round(inR,2))
-print(f"A={A:.4f}\n")
+R = float(input())
+A = 3.14159*(R**2)
+print("A={:.4f}\n".format(A))
