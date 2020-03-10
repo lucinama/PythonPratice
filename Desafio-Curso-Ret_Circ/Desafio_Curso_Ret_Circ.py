c06800c6a9a50ba9c574c8ca7226758804d67ec7
Luciana Almeida <lucianamagnoalmeida@gmail.com>
2020-03-10 20:41:44 -0300

    Better format and code

--- Desafio-Curso-Ret_Circ/Desafio_Curso_Ret_Circ.py
+++ Desafio-Curso-Ret_Circ/Desafio_Curso_Ret_Circ.py
@@ -1,25 +1,25 @@
+from math import pi
 class Retangulo:
+
     def __init__(self, ladox, ladoy):
-        self._self = self
         self.ladox = ladox
         self.ladoy = ladoy
 
     def areaRet(self):
-        return self.ladox*self.ladoy
+        return self.ladox * self.ladoy
 
     def perimRet(self):
         return 2*(self.ladox + self.ladoy)
 
+
 class Circulo:
+
     def __init__(self, raio):
         self._self = self
         self.raio = raio
 
     def areaCirc(self):
-        pi = 3.141593
-        return (self.raio**2)*pi
+        return (self.raio**2) * pi
 
     def perimCirc(self):
-        pi = 3.141593
-        return self.raio*pi*2
-    
+        return self.raio * pi * 2
